1dca8ebd55b322b22e0230de52133571f6783715
gyuha <nicegyuha@gmail.com>
2025-05-09 23:57:53 +0900

    fix: 코드 스타일 개선 및 로그 출력 주석 처리, 버전 업데이트

--- bin/show-nextjs-routers.js
+++ bin/show-nextjs-routers.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const { main } = require('../lib/index');
+const { main } = require("../lib/index");
 
 // 명령행 인자 파싱 (node와 스크립트 이름을 제외한 인자들)
 const args = process.argv.slice(2);
